9cf4a19a44fe14290daaca2a3b4f0cccaebaeb9b
thematrixxworld <amolnagare279@gmail.com>
2025-10-06 03:40:23 +0530

    added hash routing

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes'; // Your standalone routes file
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...appConfig.providers,
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    provideRouter(routes, withHashLocation()) // Use hash routing
   ]
 }).catch(err => console.error(err));
-
-function provideClipboard(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-  throw new Error('Function not implemented.');
-}
